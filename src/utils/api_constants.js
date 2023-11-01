b1b26267a8076d7130b5430f12bb578f81c08fbd
Ly Ha <20130320@st.hcmuaf.edu.vn>
2023-11-01 10:14:51 +0700

    api discount

--- src/utils/api_constants.js
+++ src/utils/api_constants.js
@@ -1,4 +1,4 @@
-const baseURL = "http://192.168.1.108:80/food_app";
+const baseURL = "http://192.168.1.46:80/food_app";
 export default ApiUrlConstants = {
   signIn: baseURL + "/authentication/sign-in.php",
   signUp: baseURL + "/authentication/sign-up.php",

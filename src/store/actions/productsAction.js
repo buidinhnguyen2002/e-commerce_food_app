e7498d7700b54da9d669c6ca2e9338a6b7dcb181
Ly Ha <20130320@st.hcmuaf.edu.vn>
2024-01-08 11:59:48 +0700

    create product

--- src/store/actions/productsAction.js
+++ src/store/actions/productsAction.js
@@ -4,3 +4,38 @@ export function saveAllProducts({ products }) {
         payload: products,
     }
 }
+export function addFood({ restaurant_id, category_id,food_name, description,price,unit,image_source ,quantity_init}) {
+    return {
+      type: "ADD_FOOD",
+      payload: {
+        restaurant_id: restaurant_id,
+        category_id:category_id,
+        food_name: food_name,
+        description: description,
+        price:price,
+        unit:unit,
+        image_source:image_source,
+        quantity_init:quantity_init,
+      },
+    };
+  }
+  export function updateFood({ restaurant_id, food_name, description,price,unit,image_source ,quantity_init}) {
+    return {
+      type: "UPDATE_FOOD",
+      payload: {
+        restaurant_id: restaurant_id,
+        food_name: food_name,
+        description: description,
+        price:price,
+        unit:unit,
+        image_source:image_source,
+        quantity_init:quantity_init,
+      },
+    };
+  }
+  export function deleteProduct({ id }) {
+    return {
+      type: "DELETE_PRODUCT",
+      payload: id,
+    };
+  }

67f8e1289598148f57b9c53680b49f8e7192c73b
Linzzz <20130306@st.hcmuaf.edu.vn>
2024-01-21 16:42:59 +0700

    merge

--- src/utils/api_constants.js
+++ src/utils/api_constants.js
@@ -1,4 +1,4 @@
-const baseURL = "http://192.168.1.81/food_app";
+const baseURL = "http://192.168.1.9/food_app";
 export default ApiUrlConstants = {
   signIn: baseURL + "/authentication/sign-in.php",
   signUp: baseURL + "/authentication/sign-up.php",
